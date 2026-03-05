dcf460c9d7398aa387eb7883c6752c1dafa8d779
santosh <santoshm.75way@gmail.com>
2026-03-05 13:00:54 +0530

    docker build

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,16 +1,19 @@
 import type { Express, Request, Response } from "express";
 import { authRoutes } from "./auth/auth.routes";
 import { prisma } from "../prisma/client";
-
+import { asyncHandler } from "./common/helper/asyncHandler";
 export function registerRoutes(app: Express) {
   app.get("/api/health", (_req: Request, res: Response) => {
     res.status(200).json({ ok: true });
   });
 
-  app.get("/api/health/db", async (_req: Request, res: Response) => {
-    await prisma.$queryRaw`SELECT 1`;
-    res.status(200).json({ ok: true, db: true });
-  });
+  app.get(
+    "/api/health/db",
+    asyncHandler(async (_req: Request, res: Response) => {
+      await prisma.$queryRaw`SELECT 1`;
+      res.status(200).json({ ok: true, db: true });
+    })
+  );
 
   app.use("/api/auth", authRoutes);
 }
